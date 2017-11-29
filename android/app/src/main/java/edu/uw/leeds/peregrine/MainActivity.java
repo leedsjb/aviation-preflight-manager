d572edf2fa25fd387fa785ad4ec44a12a8e3a37e
Jessica Libman <jlibman@gmail.com>
2017-11-29 13:08:12 -0800

    upcoming flights

--- android/app/src/main/java/edu/uw/leeds/peregrine/MainActivity.java
+++ android/app/src/main/java/edu/uw/leeds/peregrine/MainActivity.java
@@ -33,8 +33,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent i = new Intent(MainActivity.this, UpcomingFlight.class);
+                startActivity(i);
             }
         });
 
