759601f8a138f5718935f0d9af36e2f49f1e170a
Jessica Libman <jlibman@gmail.com>
2017-12-05 18:56:02 -0800

    firebase notifications

--- android/app/src/main/java/edu/uw/leeds/peregrine/ReminderMessagingService.java
+++ android/app/src/main/java/edu/uw/leeds/peregrine/ReminderMessagingService.java
@@ -1,7 +1,13 @@
 package edu.uw.leeds.peregrine;
 
 import android.app.IntentService;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -14,12 +20,13 @@ import com.google.firebase.messaging.RemoteMessage;
  * <p>
  * TODO: Customize class - update intent actions and extra parameters.
  */
-public class  ReminderMessagingService extends FirebaseMessagingService {
+public class ReminderMessagingService extends FirebaseMessagingService {
     // TODO: Rename actions, choose action names that describe tasks that this
     // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
     public static final String ACTION_FOO = "edu.uw.leeds.peregrine.action.FOO";
     public static final String ACTION_BAZ = "edu.uw.leeds.peregrine.action.BAZ";
     private static final String TAG = "MessagingService";
+    private static final String NOTIF_CHANNEL_ID = "edu.uw.leeds.peregrine.channel";
 
     // TODO: Rename parameters
     public static final String EXTRA_PARAM1 = "edu.uw.leeds.peregrine.extra.PARAM1";
@@ -66,7 +73,6 @@ public class  ReminderMessagingService extends FirebaseMessagingService {
         // TODO(developer): Handle FCM messages here.
         // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
         Log.d(TAG, "From: " + remoteMessage.getFrom());
-        Log.v(TAG, "Message received ayyyooo");
 
         // Check if message contains a data payload.
         if (remoteMessage.getData().size() > 0) {
@@ -89,5 +95,33 @@ public class  ReminderMessagingService extends FirebaseMessagingService {
 
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated. See sendNotification method below.
+        sendNotification(remoteMessage.getNotification().getBody());
+    }
+
+    /**
+     * Create and show a simple notification containing the received FCM message.
+     *
+     * @param messageBody FCM message body received.
+     */
+    private void sendNotification(String messageBody) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this, NOTIF_CHANNEL_ID)
+                        .setSmallIcon(R.drawable.ic_menu_camera) //TODO change
+                        .setContentTitle("FCM Message")
+                        .setContentText(messageBody)
+                        .setAutoCancel(true)
+                        .setSound(defaultSoundUri)
+                        .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
     }
 }
